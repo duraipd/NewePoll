f851cad7bc5741c8ca97a6e4fde1ca86925bc21c
DeepakkumarDM2002 <deepakkumar.murugesan@falms.in>
2024-01-24 17:02:16 +0530

    Login page validation

--- ePollBackend/src/main/java/com/ePoll/controller/CatsUsersController.java
+++ ePollBackend/src/main/java/com/ePoll/controller/CatsUsersController.java
@@ -1,40 +1,31 @@
 package com.ePoll.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.ePoll.model.CatsUsers;
 import com.ePoll.service.CatsUsersServiceImpl;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 
+@CrossOrigin("http://localhost:3000")
 @Controller
 public class CatsUsersController {
 	
 	@Autowired
 	private CatsUsersServiceImpl catsUSerService;
+
 	
+//	@GetMapping("/login")
+//	public String loginPage() {
+//		return "LoginPage.html";
+//	}
 
 	@GetMapping("/login")
-	public String loginPage() {
-		return "LoginPage.html";
-		
-
-	}
-
-	@GetMapping("/validation")
 	@ResponseBody
-	public String validi(CatsUsers g) {
+	public String loginValidation(CatsUsers g) {
 		
 		catsUSerService.userIDValidation(g);
 		return "Welcome";
