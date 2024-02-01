d0e13bc0af9ab2e569c80050f4db171632a12122
AnishFathima13 <anishfathima.j@falms.in>
2024-02-01 11:12:58 +0530

    dashboard submit changes

--- ePollFrontend/src/Dashboard.js
+++ ePollFrontend/src/Dashboard.js
@@ -1,3 +1,321 @@
+// import React, { useEffect, useState } from "react";
+// import "./Css/Dashboard.css";
+// import { CreateColoumn } from "./service/Service";
+// import Sidebar from "./components/Sidebar";
+// import { fetch } from "./service/Service";
+// import MyTable from "./MyTable";
+// import "./Css/table.css";
+// import { Desctable, Tablecol, tablefields } from "./service/Service";
+
+// function Dashboard() {
+//   const [fetchResponse, setFetchResponse] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [rowCount, setRowCount] = useState(1);
+//   const [tableResponse, setTableResponse] = useState([]);
+//   const [tableResponse1, setTableResponse1] = useState([]);
+//   const [selectedOption, setSelectedOption] = useState("");
+//   const [tableData, setTableData] = useState([
+//     {
+//       columnName: "",
+//       nullable: "",
+//       dataType: "",
+//       tableName: "",
+//     },
+//   ]);
+
+//   // Validation state variables
+//   const [selectedOptionError, setSelectedOptionError] = useState("");
+//   const [tableDataErrors, setTableDataErrors] = useState([]);
+//   const [formSubmitted, setFormSubmitted] = useState(false);
+
+//   const handleSelectChange = async (e) => {
+//     setSelectedOptionError("");
+//     setSelectedOption(e.target.value);
+//     const res = await tablefields(e.target.value);
+//     setTableResponse1(res);
+//     const response = await Desctable(e.target.value);
+//     setTableResponse(response);
+//     console.log(`Selected Option: ${e.target.value}`);
+//   };
+
+//   const fetchData = async () => {
+//     try {
+//       const response = await fetch();
+//       console.log("Fetched Data:", response);
+//       setFetchResponse(response);
+//     } catch (error) {
+//       console.error("Error fetching data:", error);
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchData();
+//   }, [selectedOption]);
+
+//   useEffect(() => {
+//     setTableData((prevData) =>
+//       prevData.map((row) => ({ ...row, tableName: selectedOption }))
+//     );
+//   }, [selectedOption]);
+
+//   const handleAddRow = () => {
+//     setRowCount(rowCount + 1);
+//     setTableData([
+//       ...tableData,
+//       {
+//         columnName: "",
+//         nullable: "",
+//         dataType: "",
+//         tableName: selectedOption,
+//       },
+//     ]);
+//   };
+
+//   const handleRemoveRow = (index) => {
+//     if (rowCount > 1) {
+//       setRowCount(rowCount - 1);
+//       setTableData((prevData) => prevData.filter((_, i) => i !== index));
+//     }
+//   };
+
+//   const handleChange = (event, index, field) => {
+//     const updatedData = [...tableData];
+//     updatedData[index][field] = event.target.value;
+//     setTableData(updatedData);
+//   };
+
+//   const validateForm = () => {
+//     let isValid = true;
+
+//     if (!selectedOption) {
+//       setSelectedOptionError("Please select a table");
+//       isValid = false;
+//     } else {
+//       setSelectedOptionError("");
+//     }
+
+//     const errors = tableData.map((data) => ({
+//       columnName: data.columnName.trim() === "",
+//       nullable: data.nullable.trim() === "",
+//       dataType: data.dataType.trim() === "",
+//     }));
+
+//     setTableDataErrors(errors);
+
+//     if (errors.some((error) => Object.values(error).some((e) => e))) {
+//       isValid = false;
+//     }
+
+//     return isValid;
+//   };
+
+//   const handleSubmit = async () => {
+//     setFormSubmitted(true);
+//     const isValid = validateForm();
+
+//     if (isValid) {
+//       try {
+//         const response = await Desctable(selectedOption);
+//         console.log(selectedOption);
+//         console.log(response);
+//         console.log("submit button clicked");
+//         console.log(tableData);
+//         console.log(selectedOption);
+
+//         const createColumnResponse = await CreateColoumn(tableData);
+
+//         console.log(createColumnResponse);
+
+//         alert("Table Altered Successfully");
+//       } catch (error) {
+//         console.error("Error:", error);
+//         alert("Field Already exists in the Table");
+//       }
+//     }
+//   };
+
+//   return (
+//     <div className="main">
+//       <Sidebar />
+//       <div className="container full">
+//         <div className="dashboard-container">
+//           <header className="h1table">
+//             <h6>Table Definition</h6>
+//           </header>
+//           <br></br>
+//           <div className="table-header">
+//   <div className="sub-heading1">
+//     <label htmlFor="dropdown" className="selecttable">
+//       Table:
+//     </label>
+//   </div>
+//   <div className="dropdown-container loading">
+//     {loading ? (
+//       <p>Loading options...</p>
+//     ) : (
+//       <select
+//         id="dropdown"
+//         value={selectedOption}
+//         onChange={handleSelectChange}
+//         className="form-control"
+//       >
+//         <option value="">Select table</option>
+//         {fetchResponse.map((item, index) => (
+//           <option key={index} value={item.table_name}>
+//             {item.table_name}
+//           </option>
+//         ))}
+//       </select>
+//     )}
+//     {formSubmitted && (
+//       <div className="error-message">{selectedOptionError}</div>
+//     )}
+//   </div>
+// </div>
+
+//           <main>
+//           <div className="table-container columndesign">
+//               <table>
+//                 <thead>
+//                   <tr>
+//                     <th className="headerdesign">Column Name</th>
+
+//                     <th className="headerdesign">Data Type</th>
+
+//                     <th className="headerdesign">Nullable</th>
+
+//                     <th className="headerdesign">Actions</th>
+//                   </tr>
+//                 </thead>
+
+//                 <tbody>
+//                   {tableData.map((data, index) => (
+//                     <tr key={index}>
+//                       <td>
+//                         <input
+//                           type="text"
+//                           name={`columnName${index}`}
+//                           value={data.columnName}
+//                           onChange={(e) => handleChange(e, index, "columnName")}
+//                           className="form-control"
+//                           placeholder="Column Name"
+//                           required
+//                         />
+
+//                         {formSubmitted && (
+//                           <div className="error-message">
+//                             {tableDataErrors[index]?.columnName &&
+//                               "Column Name is required"}
+//                           </div>
+//                         )}
+//                       </td>
+
+//                       <td>
+//                         <select
+//                           name={`dataType${index}`}
+//                           value={data.dataType}
+//                           onChange={(e) => handleChange(e, index, "dataType")}
+//                           className="form-control"
+//                         >
+//                           <option value="">Select the value</option>
+
+//                           <option value="Integer">Integer</option>
+
+//                           <option value="String">String</option>
+
+//                           <option value="Number">Number</option>
+
+//                           <option value="Boolean">Boolean</option>
+
+//                           <option value="Character">CHAR(n)</option>
+
+//                           <option value="Character">VARCHAR(n)</option>
+
+//                           <option value="Character">TEXT</option>
+
+//                           <option value="Date">DATE</option>
+//                         </select>
+
+//                         {formSubmitted && (
+//                           <div className="error-message">
+//                             {tableDataErrors[index]?.dataType &&
+//                               "Data Type is required"}
+//                           </div>
+//                         )}
+//                       </td>
+
+//                       <td>
+//                         <select
+//                           name={`nullable${index}`}
+//                           value={data.nullable}
+//                           onChange={(e) => handleChange(e, index, "nullable")}
+//                           className="form-control"
+//                         >
+//                           <option value="">Select the Value</option>
+
+//                           <option value="Yes">Yes</option>
+
+//                           <option value="No">No</option>
+//                         </select>
+
+//                         {formSubmitted && (
+//                           <div className="error-message ">
+//                             {tableDataErrors[index]?.nullable &&
+//                               "Nullable is required"}
+//                           </div>
+//                         )}
+//                       </td>
+
+//                       <td>
+//                         <button
+//                           className="action-button plus"
+//                           onClick={handleAddRow}
+//                         >
+//                           &#43;
+//                         </button>
+
+//                         {index > 0 && (
+//                           <>
+//                             <button
+//                               className="action-button minus"
+//                               onClick={() => handleRemoveRow(index)}
+//                             >
+//                               &#8722;
+//                             </button>
+//                           </>
+//                         )}
+//                       </td>
+//                     </tr>
+//                   ))}
+//                 </tbody>
+//               </table>
+//             </div>
+
+//             <button
+//               className="submit-button submitdesign"
+//               onClick={handleSubmit}
+//             >
+//               Submit
+//             </button>
+//           </main>
+//         </div>
+//         <MyTable tableValue={tableResponse} table={tableResponse1} />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Dashboard;
+
+
+
+
+
+
+
+
 import React, { useEffect, useState } from "react";
 import "./Css/Dashboard.css";
 import { CreateColoumn } from "./service/Service";
@@ -5,8 +323,8 @@ import Sidebar from "./components/Sidebar";
 import { fetch } from "./service/Service";
 import MyTable from "./MyTable";
 import "./Css/table.css";
-import { Desctable, Tablecol, tablefields } from "./service/Service";
-
+import { Desctable, tablefields } from "./service/Service";
+ 
 function Dashboard() {
   const [fetchResponse, setFetchResponse] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -22,26 +340,26 @@ function Dashboard() {
       tableName: "",
     },
   ]);
-
-  // Validation state variables
+ 
   const [selectedOptionError, setSelectedOptionError] = useState("");
   const [tableDataErrors, setTableDataErrors] = useState([]);
   const [formSubmitted, setFormSubmitted] = useState(false);
-
+  const [refreshKey, setRefreshKey] = useState(0); // New state for remounting MyTable
+ 
   const handleSelectChange = async (e) => {
     setSelectedOptionError("");
     setSelectedOption(e.target.value);
+ 
     const res = await tablefields(e.target.value);
     setTableResponse1(res);
+ 
     const response = await Desctable(e.target.value);
     setTableResponse(response);
-    console.log(`Selected Option: ${e.target.value}`);
   };
-
+ 
   const fetchData = async () => {
     try {
       const response = await fetch();
-      console.log("Fetched Data:", response);
       setFetchResponse(response);
     } catch (error) {
       console.error("Error fetching data:", error);
@@ -49,19 +367,20 @@ function Dashboard() {
       setLoading(false);
     }
   };
-
+ 
   useEffect(() => {
     fetchData();
   }, [selectedOption]);
-
+ 
   useEffect(() => {
     setTableData((prevData) =>
       prevData.map((row) => ({ ...row, tableName: selectedOption }))
     );
   }, [selectedOption]);
-
+ 
   const handleAddRow = () => {
     setRowCount(rowCount + 1);
+ 
     setTableData([
       ...tableData,
       {
@@ -72,70 +391,76 @@ function Dashboard() {
       },
     ]);
   };
-
+ 
   const handleRemoveRow = (index) => {
     if (rowCount > 1) {
       setRowCount(rowCount - 1);
       setTableData((prevData) => prevData.filter((_, i) => i !== index));
     }
   };
-
+ 
   const handleChange = (event, index, field) => {
     const updatedData = [...tableData];
     updatedData[index][field] = event.target.value;
     setTableData(updatedData);
   };
-
+ 
   const validateForm = () => {
     let isValid = true;
-
+ 
     if (!selectedOption) {
       setSelectedOptionError("Please select a table");
       isValid = false;
     } else {
       setSelectedOptionError("");
     }
-
+ 
     const errors = tableData.map((data) => ({
       columnName: data.columnName.trim() === "",
       nullable: data.nullable.trim() === "",
       dataType: data.dataType.trim() === "",
     }));
-
+ 
     setTableDataErrors(errors);
-
+ 
     if (errors.some((error) => Object.values(error).some((e) => e))) {
       isValid = false;
     }
-
+ 
     return isValid;
   };
-
+ 
+  const resetFormData = () => {
+    setTableData([
+      {
+        columnName: "",
+        nullable: "",
+        dataType: "",
+        tableName: selectedOption,
+      },
+    ]);
+  };
+ 
   const handleSubmit = async () => {
     setFormSubmitted(true);
     const isValid = validateForm();
-
+ 
     if (isValid) {
       try {
         const response = await Desctable(selectedOption);
-        console.log(selectedOption);
-        console.log(response);
-        console.log("submit button clicked");
-        console.log(tableData);
-        console.log(selectedOption);
-
         const createColumnResponse = await CreateColoumn(tableData);
-
-        console.log(createColumnResponse);
-
+ 
         alert("Table Altered Successfully");
+ 
+        resetFormData();
+        setRefreshKey((prevKey) => prevKey + 1);
       } catch (error) {
         console.error("Error:", error);
         alert("Field Already exists in the Table");
       }
     }
   };
-
+ 
   return (
     <div className="main">
       <Sidebar />
@@ -145,14 +470,15 @@ function Dashboard() {
             <h6>Table Definition</h6>
           </header>
           <br></br>
-          <div className="table-header">
-  <div className="sub-heading1">
-    <label htmlFor="dropdown" className="selecttable">
-      Table:
-    </label>
-  </div>
-  <div className="dropdown-container loading">
-    {loading ? (
+          
+           <div className="table-header">
+   <div className="sub-heading1">
+     <label htmlFor="dropdown" className="selecttable">
+       Table:
+     </label>
+   </div>
+   <div className="dropdown-container loading">
+     {loading ? (
       <p>Loading options...</p>
     ) : (
       <select
@@ -174,22 +500,17 @@ function Dashboard() {
     )}
   </div>
 </div>
-
           <main>
-          <div className="table-container columndesign">
+            <div className="table-container columndesign">
               <table>
                 <thead>
                   <tr>
                     <th className="headerdesign">Column Name</th>
-
                     <th className="headerdesign">Data Type</th>
-
                     <th className="headerdesign">Nullable</th>
-
                     <th className="headerdesign">Actions</th>
                   </tr>
                 </thead>
-
                 <tbody>
                   {tableData.map((data, index) => (
                     <tr key={index}>
@@ -203,7 +524,6 @@ function Dashboard() {
                           placeholder="Column Name"
                           required
                         />
-
                         {formSubmitted && (
                           <div className="error-message">
                             {tableDataErrors[index]?.columnName &&
@@ -211,7 +531,6 @@ function Dashboard() {
                           </div>
                         )}
                       </td>
-
                       <td>
                         <select
                           name={`dataType${index}`}
@@ -220,24 +539,15 @@ function Dashboard() {
                           className="form-control"
                         >
                           <option value="">Select the value</option>
-
                           <option value="Integer">Integer</option>
-
                           <option value="String">String</option>
-
                           <option value="Number">Number</option>
-
                           <option value="Boolean">Boolean</option>
-
                           <option value="Character">CHAR(n)</option>
-
                           <option value="Character">VARCHAR(n)</option>
-
                           <option value="Character">TEXT</option>
-
                           <option value="Date">DATE</option>
                         </select>
-
                         {formSubmitted && (
                           <div className="error-message">
                             {tableDataErrors[index]?.dataType &&
@@ -245,7 +555,6 @@ function Dashboard() {
                           </div>
                         )}
                       </td>
-
                       <td>
                         <select
                           name={`nullable${index}`}
@@ -254,12 +563,9 @@ function Dashboard() {
                           className="form-control"
                         >
                           <option value="">Select the Value</option>
-
                           <option value="Yes">Yes</option>
-
                           <option value="No">No</option>
                         </select>
-
                         {formSubmitted && (
                           <div className="error-message ">
                             {tableDataErrors[index]?.nullable &&
@@ -267,7 +573,6 @@ function Dashboard() {
                           </div>
                         )}
                       </td>
-
                       <td>
                         <button
                           className="action-button plus"
@@ -275,7 +580,6 @@ function Dashboard() {
                         >
                           &#43;
                         </button>
-
                         {index > 0 && (
                           <>
                             <button
@@ -292,7 +596,6 @@ function Dashboard() {
                 </tbody>
               </table>
             </div>
-
             <button
               className="submit-button submitdesign"
               onClick={handleSubmit}
@@ -301,11 +604,17 @@ function Dashboard() {
             </button>
           </main>
         </div>
-        <MyTable tableValue={tableResponse} table={tableResponse1} />
+        <br></br>
+        <MyTable
+          tableValue={tableResponse}
+          table={tableResponse1}
+          resetFormData={resetFormData}
+          key={refreshKey}
+        />
       </div>
     </div>
   );
 }
-
+ 
 export default Dashboard;
 
